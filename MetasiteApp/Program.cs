bc1ffd8aa2e2464dfa5ff1e99a0efcba938911f6
juliuspar <julius.paradnikas@gmail.com>
2018-12-25 19:16:53 +0200

    Add migrations

--- MetasiteApp/Program.cs
+++ MetasiteApp/Program.cs
@@ -1,14 +1,19 @@
+using System;
+using MetasiteApp.Phones.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace MetasiteApp
 {
     public class Program
     {
         public static void Main(string[] args) => 
+            BuildWebHost(args).Run();
+
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args).
             UseStartup<Startup>().
-            Build().
-            Run();
+            Build();
     }
 }
